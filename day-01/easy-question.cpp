4830f20e01bd1a9ce19a5b4af9d966cdb0386ff5
Sameer-Amity-Uni <sameer.hussain@s.amity.edu>
2024-09-28 23:36:57 +0530

    Create easy-question.cpp

---
+++ day-01/easy-question.cpp
@@ -0,0 +1,18 @@
+// Cricket World Cup Qualifier
+/* The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 12
+12 or more points in the group stage matches qualifies for the next stage. */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+	cin>>X;
+	if(X>=12){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+	
+	return 0;
+}
